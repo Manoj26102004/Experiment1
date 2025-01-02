b8837e132b92ef5230583823491053eab2d58a1d
Manoj S P <msaligra@gitam.in>
2025-01-02 12:37:13 +0530

    v3 commit

--- v1.py
+++ v1.py
@@ -1,9 +1,13 @@
-a = float(input("Enter the value of a :"))
-b = float(input("Enter the value of b :"))
-c = float(input("Enter the value of c :"))
-x = float(input("Enter the value of x :"))
-y=a*(x**2)+b*x+c
-print("\nVersion 2\n")
-print("Quadratic Weather Model")
-print(f"Equation: y = {a}x^2+{b}x={c}")
-print(f"Prediction for x={x}: y={y}n");
+def weathering_model():
+    with open('input_single.txt','r') as file:
+        lines = file.readlines()
+        a = float(lines[0])
+        b = float(lines[1])
+        c = float(lines[2])
+        x = float(lines[3])
+        y=a*(x**2)+b*x+c
+        print("\nVersion 3\n")
+        print("Quadratic Weather Model")
+        print(f"Equation: y = {a}x^2+{b}x={c}")
+        print(f"Prediction for x={x}: y={y}\n");
+weathering_model()
